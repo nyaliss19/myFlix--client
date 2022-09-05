54aa63fb525aeaccc35bd4d8c59d45c141bc30ca
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-09-05 19:26:09 -0400

    Added all movies from API

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33124,10 +33124,15 @@ var MovieCard = /*#__PURE__*/function (_React$Component) {
   _createClass(MovieCard, [{
     key: "render",
     value: function render() {
-      var movie = this.props.movie;
+      var _this$props = this.props,
+          movie = _this$props.movie,
+          onMovieClick = _this$props.onMovieClick;
       return /*#__PURE__*/_react.default.createElement("div", {
-        className: "movie-card"
-      }, "some Title");
+        className: "movie-card",
+        onClick: function onClick() {
+          onMovieClick(movie);
+        }
+      }, movie.Title);
     }
   }]);
 
@@ -33135,17 +33140,118 @@ var MovieCard = /*#__PURE__*/function (_React$Component) {
 }(_react.default.Component);
 
 exports.MovieCard = MovieCard;
+},{"react":"../node_modules/react/index.js"}],"components/movie-view/movie-view.jsx":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.MovieView = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var MovieView = /*#__PURE__*/function (_React$Component) {
+  _inherits(MovieView, _React$Component);
+
+  var _super = _createSuper(MovieView);
+
+  function MovieView() {
+    _classCallCheck(this, MovieView);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(MovieView, [{
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          movie = _this$props.movie,
+          onBackClick = _this$props.onBackClick;
+      return /*#__PURE__*/_react.default.createElement("div", {
+        className: "movie-view"
+      }, /*#__PURE__*/_react.default.createElement("div", {
+        className: "movie-poster"
+      }, /*#__PURE__*/_react.default.createElement("img", {
+        src: movie.ImagePath
+      })), /*#__PURE__*/_react.default.createElement("div", {
+        className: "movie-title"
+      }, /*#__PURE__*/_react.default.createElement("span", {
+        className: "label"
+      }, "Title: "), /*#__PURE__*/_react.default.createElement("span", {
+        className: "value"
+      }, movie.Title)), /*#__PURE__*/_react.default.createElement("div", {
+        className: "movie-description"
+      }, /*#__PURE__*/_react.default.createElement("span", {
+        className: "label"
+      }, "Description: "), /*#__PURE__*/_react.default.createElement("span", {
+        className: "value"
+      }, movie.Description)), /*#__PURE__*/_react.default.createElement("div", {
+        className: "movie-genre"
+      }, /*#__PURE__*/_react.default.createElement("span", {
+        className: "label"
+      }, "Genre: "), /*#__PURE__*/_react.default.createElement("span", {
+        className: "value"
+      }, movie.Genre)), /*#__PURE__*/_react.default.createElement("div", {
+        className: "movie-director"
+      }, /*#__PURE__*/_react.default.createElement("span", {
+        className: "label"
+      }, "Director: "), /*#__PURE__*/_react.default.createElement("span", {
+        className: "value"
+      }, movie.Director)), /*#__PURE__*/_react.default.createElement("div", {
+        className: "movie-featured"
+      }, /*#__PURE__*/_react.default.createElement("span", {
+        className: "label"
+      }, "Featured: "), /*#__PURE__*/_react.default.createElement("span", {
+        className: "value"
+      }, movie.Featured)), /*#__PURE__*/_react.default.createElement("button", {
+        onClick: function onClick() {
+          onBackClick(null);
+        }
+      }, "Back"));
+    }
+  }]);
+
+  return MovieView;
+}(_react.default.Component);
+
+exports.MovieView = MovieView;
 },{"react":"../node_modules/react/index.js"}],"components/main-view/main-view.jsx":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.MainView = void 0;
+exports.default = exports.MainView = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
 var _movieCard = require("../movie-card/movie-card");
+
+var _movieView = require("../movie-view/movie-view");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -33177,24 +33283,130 @@ var MainView = /*#__PURE__*/function (_React$Component) {
   var _super = _createSuper(MainView);
 
   function MainView() {
+    var _this;
+
     _classCallCheck(this, MainView);
 
-    return _super.apply(this, arguments);
+    _this = _super.call(this);
+    _this.state = {
+      movies: [{
+        _id: 1,
+        Title: "The Manchurian Candidate",
+        Description: "Desc1...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 2,
+        Title: "The Chronicles of Riddick",
+        Description: "Desc2...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 3,
+        Title: "Dune Part One",
+        Description: "Desc3...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 4,
+        Title: "Arrival",
+        Description: "Desc4...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 5,
+        Title: "Riddick",
+        Description: "Desc5...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 6,
+        Title: "Ex Machina",
+        Description: "Desc6...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 7,
+        Title: "Annihilation",
+        Description: "Desc7...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 8,
+        Title: "Awakenings",
+        Description: "Desc8...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 9,
+        Title: "40 Year Old Virgin",
+        Description: "Desc9...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }, {
+        _id: 10,
+        Title: "Silence of the Lambs",
+        Description: "Desc10...",
+        Genre: "genre",
+        Director: "director",
+        ImagePath: "...",
+        Featured: "true"
+      }],
+      selectedMovie: null
+    };
+    return _this;
   }
 
   _createClass(MainView, [{
+    key: "setSelectedMovie",
+    value: function setSelectedMovie(newSelectedMovie) {
+      this.setState({
+        selectedMovie: newSelectedMovie
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
-      var movies = this.state.movies;
+      var _this2 = this;
+
+      var _this$state = this.state,
+          movies = _this$state.movies,
+          selectedMovie = _this$state.selectedMovie;
       if (movies.length === 0) return /*#__PURE__*/_react.default.createElement("div", {
         className: "main-view"
       }, "The list is empty!");
       return /*#__PURE__*/_react.default.createElement("div", {
         className: "main-view"
-      }, movies.map(function (movie) {
+      }, selectedMovie ? /*#__PURE__*/_react.default.createElement(_movieView.MovieView, {
+        movie: selectedMovie,
+        onBackClick: function onBackClick(newSelectedMovie) {
+          _this2.setSelectedMovie(newSelectedMovie);
+        }
+      }) : movies.map(function (movie) {
         return /*#__PURE__*/_react.default.createElement(_movieCard.MovieCard, {
           key: movie._id,
-          movie: movie
+          movie: movie,
+          onMovieClick: function onMovieClick(movie) {
+            _this2.setSelectedMovie(movie);
+          }
         });
       }));
     }
@@ -33204,7 +33416,9 @@ var MainView = /*#__PURE__*/function (_React$Component) {
 }(_react.default.Component);
 
 exports.MainView = MainView;
-},{"react":"../node_modules/react/index.js","../movie-card/movie-card":"components/movie-card/movie-card.jsx"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var _default = MainView;
+exports.default = _default;
+},{"react":"../node_modules/react/index.js","../movie-card/movie-card":"components/movie-card/movie-card.jsx","../movie-view/movie-view":"components/movie-view/movie-view.jsx"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -33365,7 +33579,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52879" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50591" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,111 @@ import React from "react";
 import { MovieCard } from "../movie-card/movie-card";
 
 export class MainView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        {
+          _id: 1,
+          Title: "The Manchurian Candidate",
+          Description: "Desc1...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 2,
+          Title: "The Chronicles of Riddick",
+          Description: "Desc2...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 3,
+          Title: "Dune Part One",
+          Description: "Desc3...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 4,
+          Title: "Arrival",
+          Description: "Desc4...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 5,
+          Title: "Riddick",
+          Description: "Desc5...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 6,
+          Title: "Ex Machina",
+          Description: "Desc6...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 7,
+          Title: "Annihilation",
+          Description: "Desc7...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 8,
+          Title: "Awakenings",
+          Description: "Desc8...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 9,
+          Title: "40 Year Old Virgin",
+          Description: "Desc9...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+        {
+          _id: 10,
+          Title: "Silence of the Lambs",
+          Description: "Desc10...",
+          Genre: "genre",
+          Director: "director",
+          ImagePath: "...",
+          Featured: "true",
+        },
+      ],
+      selectedMovie: null,
+    };
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie,
+    });
+  }
+
   render() {
     const { movies } = this.state;
 
@@ -17,3 +122,5 @@ export class MainView extends React.Component {
     );
   }
 }
+
+export default MainView;

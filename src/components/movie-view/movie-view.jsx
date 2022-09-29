7ff5bfeccbd58427bf2315127d9180c5f62c2d09
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-09-29 00:31:01 -0400

    Update movie-view.jsx
    Case sensitive

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -28,11 +28,11 @@ export class MovieView extends React.Component {
         </div>
         <div className='movie-genre'>
           <span className='label'>Genre: </span>
-          <span className='value'>{movie.Genre.name}</span>
+          <span className='value'>{movie.Genre.Name}</span>
         </div>
         <div className='movie-director'>
           <span className='label'>Director: </span>
-          <span className='value'>{movie.Director.name}</span>
+          <span className='value'>{movie.Director.Name}</span>
         </div>
         <div className='movie-featured'>
           <span className='label'>Featured: </span>

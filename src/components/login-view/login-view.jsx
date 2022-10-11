60adef0d177171c31604cf32f60bf45b6e6387cf
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-10-10 23:03:19 -0400

    Update login-view.jsx
    refactoring

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,96 +8,110 @@ import Col from 'react-bootstrap/Col';
 
 import './login-view.scss';
 
-export function LoginView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+export class LoginView extends React.Component {
+  constructor(props) {
+    super();
+    this.form = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onLoggedIn = props.onLoggedIn;
+    this.handleClickRegister = this.handleClickRegister.bind(this);
+    this.toRegistrationView = props.toRegistrationView;
+  }
 
-  const handleSubmit = (e) => {
+  handleSubmit() {
+    if (this.form.current.reportValidity()) {
+      let loginCredentials = {
+        Username: this.form.current[0].value,
+        Password: this.form.current[1].value,
+      };
+
+      // Send auth request to server
+      axios
+        .post('https://rcarpus-movie-api.herokuapp.com/login', loginCredentials)
+        .then((response) => {
+          const data = response.data;
+          this.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log('no such user');
+          window.alert('invalid username or password');
+        });
+    }
+  }
+
+  handleClickRegister(e) {
     e.preventDefault();
-    /* Send a request to the server for authentication */
-    Axios.post('https://nyaliss-flix-27.herokuapp.com/login', {
-      Username: username,
-      Password: password,
-    })
-      .then((response) => {
-        const data = response.data;
-        props.onLoggedIn(data);
-      })
-      .catch((e) => {
-        console.log('no such user');
-      });
-  };
+    // LOOK FOR A WAY TO REFACTOR THIS SO THAT I DO NOT NEED THIS USELESS PARAMETER
+    this.toRegistrationView('');
+  }
 
-  const handleClickRegister = (e) => {
-    e.preventDefault();
-    props.toRegistrationView('');
-  };
-
-  return (
-    <div className='login-view'>
-      <Row>
-        <Col>
-          <h2 className='display-4'>Login to myFlix</h2>
-        </Col>
-      </Row>
-
-      <Form className='login-form'>
-        <Form.Group controlId='formUsername'>
-          <Row className='login-view__line'>
-            <Col sm={0} md={3}></Col>
-            <Col sm={12} md={2}>
-              <Form.Label>Username:</Form.Label>
-            </Col>
-            <Col sm={12} md={4}>
-              <Form.Control
-                type='text'
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-
-        <Form.Group controlId='formPassword'>
-          <Row className='login-view__line'>
-            <Col sm={0} md={3}></Col>
-            <Col sm={12} md={2}>
-              <Form.Label>Password:</Form.Label>
-            </Col>
-            <Col sm={12} md={4}>
-              <Form.Control
-                type='text'
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-
-        <Row className='login-view__line'>
-          <Col md={8}></Col>
+  render() {
+    return (
+      <div className='login-view'>
+        <Row>
           <Col>
-            <Button variant='primary' type='submit' onClick={handleSubmit}>
-              Log in
+            <h2 className='display-4'>Login to myFlix</h2>
+          </Col>
+        </Row>
+
+        <Form className='login-form'>
+          <Form.Group controlId='formUsername'>
+            <Row className='login-view__line'>
+              <Col sm={0} md={3}></Col>
+              <Col sm={12} md={2}>
+                <Form.Label>Username:</Form.Label>
+              </Col>
+              <Col sm={12} md={4}>
+                <Form.Control
+                  type='text'
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Form.Group controlId='formPassword'>
+            <Row className='login-view__line'>
+              <Col sm={0} md={3}></Col>
+              <Col sm={12} md={2}>
+                <Form.Label>Password:</Form.Label>
+              </Col>
+              <Col sm={12} md={4}>
+                <Form.Control
+                  type='text'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Row className='login-view__line'>
+            <Col md={8}></Col>
+            <Col>
+              <Button variant='primary' type='submit' onClick={handleSubmit}>
+                Log in
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+
+        <Row className='register-row'>
+          <Col md={3}>
+            <p>Don't have an account? </p>
+          </Col>
+          <Col>
+            <Button
+              variant='secondary'
+              type='submit'
+              onClick={handleClickRegister}
+            >
+              Register
             </Button>
           </Col>
         </Row>
-      </Form>
-
-      <Row className='register-row'>
-        <Col md={3}>
-          <p>Don't have an account? </p>
-        </Col>
-        <Col>
-          <Button
-            variant='secondary'
-            type='submit'
-            onClick={handleClickRegister}
-          >
-            Register
-          </Button>
-        </Col>
-      </Row>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 LoginView.propTypes = {

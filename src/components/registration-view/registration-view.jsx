de809a536496172ad60fbccc7429df49c475ad9d
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-10-13 23:11:52 -0400

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Axios from 'axios';
 import PropTypes from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,156 +7,193 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import './registration-view.scss';
+import { render } from 'react-dom';
 
-export function RegistrationView(props) {
-  const [username, setUsername] = useState('');
-  const [password1, setPassword1] = useState('');
-  const [password2, setPassword2] = useState('');
-  const [email, setEmail] = useState('');
-  const [birthday, setBirthday] = useState('');
+export class RegistrationView extends React.Component {
+  constructor(props) {
+    super();
+    this.form = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.onRegister(true, username);
-  };
+  //modify state of MainView to be registered and logged in with new user
+  handleSubmit() {
+    //send auth request
+    let newUserInfo = {};
+    if (
+      this.form.current.reportValidity() &&
+      this.form.current[1].value !== this.form.current[2].value
+    ) {
+      window.alert('passwords must match');
+    }
+    if (
+      this.form.current.reportValidity() &&
+      this.form.current[1].value === this.form.current[2].value
+    ) {
+      console.log('adding a new user');
+      newUserInfo.Username = this.form.current[0].value;
+      newUserInfo.Password = this.form.current[1].value;
+      newUserInfo.Email = this.form.current[3].value;
+      if (this.form.current[4].value !== '') {
+        newUserInfo.Birthday = this.form.current[4].value;
+      }
+      Axios.post(
+        'https://nyaliss-flix-27.herokuapp.com/users/register',
+        newUserInfo
+      )
+        .then((response) => {
+          const data = response.data;
+          console.log('registered successful');
+          console.log(data);
+          window.open('/', '_self');
+        })
+        .catch((e) => {
+          console.log('something went wrong. Maybe some info was missing.');
+        });
+    } else {
+      console.log('failed to fill out form properly');
+    }
+  }
 
-  let labelSize = 4;
-  let fieldSize = 5;
-  let emptySize = 1;
+  render() {
+    let labelSize = 4;
+    let fieldSize = 5;
+    let emptySize = 1;
 
-  return (
-    <div className='registration-view'>
-      <Row>
-        <Col>
-          <h2 className='display-4'>Sign up for a free MyFlix account</h2>
-        </Col>
-      </Row>
-
-      <Form className='registration-form'>
-        <Form.Group className='registration-form__line'>
-          <Col md={emptySize}></Col>
-          <Col md={labelSize}>
-            <Form.Label className='registration-form__line-label'>
-              Username:{' '}
-              <span className='registration-form__label-tips'>
-                (5+ characters, no spaces)
-              </span>
-            </Form.Label>
-          </Col>
-          <Col md={fieldSize}>
-            <Form.Control
-              className='registration-form__line__input-field'
-              type='text'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Col>
-        </Form.Group>
-
-        <Form.Group className='registration-form__line'>
-          <Row>
-            <Col md={emptySize}></Col>
-            <Col md={labelSize}>
-              <Form.Label className='registration-form__line-label'>
-                Enter desired password{' '}
-                <span className='registration-form__label-tips'>
-                  (must not be blank)
-                </span>
-              </Form.Label>
-            </Col>
-            <Col md={labelSize}>
-              <Form.Control
-                className='registration-form__line__input-field'
-                type='text'
-                value={password1}
-                onChange={(e) => setPassword1(e.target.value)}
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-
-        <Form.Group className='registration-form__line'>
-          <Row>
-            <Col md={emptySize}></Col>
-            <Col md={labelSize}>
-              <Form.Label className='registration-form__line-label'>
-                Re-enter password:{' '}
-                <span className='registration-form__label-tips'>
-                  passwords must match
-                </span>
-              </Form.Label>
-            </Col>
-            <Col md={fieldSize}>
-              <Form.Control
-                className='registration-form__line__input-field'
-                type='text'
-                value={password2}
-                onChange={(e) => setPassword2(e.target.value)}
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-
-        <Form.Group className='registration-form__line'>
-          <Row>
-            <Col md={emptySize}></Col>
-            <Col md={labelSize}>
-              <Form.Label className='registration-form__line-label'>
-                Email:{' '}
-                <span className='registration-form__label-tips'>
-                  (required)
-                </span>
-              </Form.Label>
-            </Col>
-            <Col md={fieldSize}>
-              <Form.Control
-                className='registration-form__line__input-field'
-                type='text'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-
-        <Form.Group className='registration-form__line'>
-          <Row>
-            <Col md={emptySize}></Col>
-            <Col md={labelSize}>
-              <Form.Label className='registration-form__line-label'>
-                Birthday:{' '}
-                <span className='registration-form__label-tips'>
-                  (optional)
-                </span>
-              </Form.Label>
-            </Col>
-            <Col md={fieldSize}>
-              <Form.Control
-                className='registration-form__line__input-field'
-                type='text'
-                value={birthday}
-                onChange={(e) => setBirthday(e.target.value)}
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-
+    return (
+      <div className='registration-view'>
         <Row>
-          <Col md={labelSize + fieldSize + emptySize - 2}></Col>
-          <Col md={1}>
-            <Button
-              className='register-button'
-              variant='primary'
-              type='submit'
-              onClick={handleSubmit}
-            >
-              Register
-            </Button>
+          <Col>
+            <h2 className='display-4'>Sign up for a free MyFlix account</h2>
           </Col>
         </Row>
-      </Form>
-    </div>
-  );
+
+        <Form
+          className='registration-form'
+          ref={this.form}
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <Form.Group className='registration-form__line'>
+            <Row>
+              <Col md={emptySize}></Col>
+              <Col md={labelSize}>
+                <Form.Label className='registration-form__line-label'>
+                  Username:{' '}
+                  <span className='registration-form__label-tips'>
+                    (5+ characters, no spaces)
+                  </span>
+                </Form.Label>
+              </Col>
+              <Col md={fieldSize}>
+                <Form.Control
+                  className='registration-form__line__input-field'
+                  pattern='^[a-zA-Z0-9]{5,}$'
+                  required
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Form.Group className='registration-form__line'>
+            <Row>
+              <Col md={emptySize}></Col>
+              <Col md={labelSize}>
+                <Form.Label className='registration-form__line-label'>
+                  Enter desired password{' '}
+                  <span className='registration-form__label-tips'>
+                    (must not be blank)
+                  </span>
+                </Form.Label>
+              </Col>
+              <Col md={fieldSize}>
+                <Form.Control
+                  className='registration-form__line__input-field'
+                  type='password'
+                  pattern='.+'
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Form.Group className='registration-form__line'>
+            <Row>
+              <Col md={emptySize}></Col>
+              <Col md={labelSize}>
+                <Form.Label className='registration-form__line-label'>
+                  Re-enter password:{' '}
+                  <span className='registration-form__label-tips'>
+                    passwords must match
+                  </span>
+                </Form.Label>
+              </Col>
+              <Col md={fieldSize}>
+                <Form.Control
+                  className='registration-form__line__input-field'
+                  type='password'
+                  pattern='.+'
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Form.Group className='registration-form__line'>
+            <Row>
+              <Col md={emptySize}></Col>
+              <Col md={labelSize}>
+                <Form.Label className='registration-form__line-label'>
+                  Email:{' '}
+                  <span className='registration-form__label-tips'>
+                    (required)
+                  </span>
+                </Form.Label>
+              </Col>
+              <Col md={fieldSize}>
+                <Form.Control
+                  className='registration-form__line__input-field'
+                  pattern='.*@.*\..*'
+                  required
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Form.Group className='registration-form__line'>
+            <Row>
+              <Col md={emptySize}></Col>
+              <Col md={labelSize}>
+                <Form.Label className='registration-form__line-label'>
+                  Birthday:{' '}
+                  <span className='registration-form__label-tips'>
+                    (optional)
+                  </span>
+                </Form.Label>
+              </Col>
+              <Col md={fieldSize}>
+                <Form.Control
+                  className='registration-form__line__input-field'
+                  pattern='^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Row>
+            <Col md={labelSize + fieldSize + emptySize - 2}></Col>
+            <Col md={1}>
+              <Button
+                className='register-button'
+                variant='primary'
+                type='submit'
+                onClick={this.handleSubmit}
+              >
+                Register
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    );
+  }
 }
 
 RegistrationView.propTypes = {
